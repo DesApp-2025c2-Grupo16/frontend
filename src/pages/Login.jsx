240bdc9dc15bde23d5e5f1867fd543c1942fb6c9
Marco-Musillario-U <marcosantino.musillarioaguilar@estudiantes.unahur.edu.ar>
2025-12-02 17:37:50 -0300

    Arreglado que se navegaba al dashboard antes de setear prestadores en local storage si el usuario logueado es un centro

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,16 +26,16 @@ export default function Login() {
         setInvalid(false)
         const user = await res.json()
         localStorage.setItem("auth_user", JSON.stringify(user))
-        navigate("/dashboard");
-
+        
         if(!user.esCentro){
-        localStorage.setItem("prestadorId", user.id)
+          localStorage.setItem("prestadorId", user.id)
         } else {
-        fetch(`http://localhost:3001/prestadores/medicos/${user.id}`)
+          fetch(`http://localhost:3001/prestadores/medicos/${user.id}`)
           .then(r => r.json())
           .then(medicos => {
             localStorage.setItem("prestadores", JSON.stringify(medicos))
             localStorage.setItem("prestadorId", medicos[0].id)
+            navigate("/dashboard");
           })
         }
       }
